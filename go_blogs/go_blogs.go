b51f24ecb17cf09bbc6e12e397f9e1cd780a1522
devin7788 <andy129408@gmail.com>
2020-11-16 19:21:06 +0800

    update

--- go_blogs/go_blogs.go
+++ go_blogs/go_blogs.go
@@ -3,6 +3,7 @@ package go_blogs
 func RecordGoBlogs() {
 	var httpUrl = []string{
 		"https://www.golanglearn.com/",
+		"https://colobu.com/",
 	}
 	for _, v := range httpUrl {
 		println(v)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "godeepdive/go_blogs"
+
+func main() {
+	go_blogs.RecordGoBlogs()
+}
